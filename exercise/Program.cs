036dbc9b88d830b1823c2fa294f880148c32771c
hp <gbhargavi86@gmail.com>
2023-11-08 18:45:42 -0800

    Add project files.

---
+++ exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+            Console.WriteLine("Enter yourname:");
+            string yourname=Console.ReadLine();
+            Console.WriteLine("yourname is "+ yourname);
+            double myDouble= 9.22;
+            int myInt= (int) myDouble; //Explicit type casting
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myInt);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine(); //to print username in run time
+            Console.WriteLine("Username is: " + userName); // to print username is: aboveusername
+            Console.Write("Hello bujji "); // to print in sameline
+            Console.Write("welcome to vegas");
+            int myNum = 15;
+            myNum = 20;
+            Console.WriteLine(myNum);
+
+        }
+    }
+}
